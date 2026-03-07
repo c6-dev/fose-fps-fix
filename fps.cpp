deb452293998cb6d4d7585568baabcf22407edeb
c6 <31777460+c6-dev@users.noreply.github.com>
2026-03-07 12:18:38 +0400

    remove work in progress patches

--- fps.cpp
+++ fps.cpp
@@ -132,29 +132,4 @@ void WritePatches() {
 
 	WriteRelJump(0x6EED54, UInt32(FastExit));
 
-
-	SafeWrite32(0x448589, 26417); // // NiTMap<unsigned int,TESFile *> - Owned Temp ID Map (ONAM)
-	// TODO NV resized hashmaps
-/*
-
-	SafeWrite8(0x544FA7, 41);		// NiTMap<TESObjectREFR *,NiNode *> - TESObjectCELL's animated references
-	SafeWrite8(0x544FC9, 29);		// NiTMap<TESObjectREFR *,NiNode *> - TESObjectCELL's external emittance objects
-
-	SafeWrite8(0x582CA2, 127);		// NiTPointerMap<unsigned int,BSSimpleList<TESObjectREFR *> *> - TESWorldSpace's fixed persistent references
-	SafeWrite8(0x582CEF, 53);		// NiTMap<TESFile *,TESWorldSpace::OFFSET_DATA *> - TESWorldSpace's file offset map
-	SafeWrite32(0x583FF6, 1709);	// NiTPointerMap<int,TESObjectCELL *> - TESWorldSpace's cell map (form loader)
-	SafeWrite8(0x582D64, 31);		// NiTPointerMap<int,TESObjectCELL *> - TESWorldSpace's cell map (constructor)
-	SafeWrite8(0x587AC9, 43);		// NiTPointerMap<unsigned int,BSSimpleList<TESObjectREFR *> *> - TESWorldSpace's overlapped multibounds
-
-	SafeWrite8(0x6C02F8, 127);		// NiTMap<unsigned int,NiPointer<ReferenceObstacleArray>> - NavMeshObstacleManager's obstacle map
-	SafeWrite8(0x6C035F, 97);		// NiTMap<bhkRigidBody *,NiPointer<ObstacleData>> - NavMeshObstacleManager's obstacle data map
-	SafeWrite8(0x6C0397, 97);		// NiTMap<unsigned int,NiPointer<ReferenceObstacleArray>> - NavMeshObstacleManager's open doors map
-	SafeWrite8(0x6C03AB, 89);		// NiTMap<unsigned int,NiPointer<ReferenceObstacleArray>> - NavMeshObstacleManager's closed doors map
-
-	SafeWrite8(0x6E13AF, 53);		// NiTMap<unsigned int,unsigned int> - PathingLOSMap
-
-	SafeWrite8(0xAD9169, 113);		// NiTMapBase<NiTPointerAllocator<unsigned int>,int,BSGameSound *> - BSAudioManager's playing sounds
-	SafeWrite8(0xAD9189, 113);		// NiTMapBase<NiTPointerAllocator<unsigned int>,int,BSSoundInfo *> - BSAudioManager's playing sound infos
-	SafeWrite8(0xAD91CC, 41);		// NiTMapBase<NiTPointerAllocator<unsigned int>,int,NiPointer<NiAVObject>> - BSAudioManager's moving sounds
-	*/
 }
